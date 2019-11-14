41d89043e737b4d471a14763c5b4a497080c0ee8
Tom MARAVAL <tmaraval@e1r13p2.42.fr>
2019-11-14 11:26:18 +0100

    wip test

--- tests/test_ft_memcpy.c
+++ tests/test_ft_memcpy.c
@@ -6,46 +6,42 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 14:43:36 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/11/11 10:33:33 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/11/14 10:35:14 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-void	test_ft_memcpy_2(void)
+void	test_ft_memcpy(void)
 {
-	char s1[250];
-	char s2[250];
+	char *src;
+	char *destft;
+	char *destmemcpy;
 	int i;
+	int size;
 
+
+	size = 0;
 	printf(" [*] test on : %s\n", __FUNCTION__);
 	i = 0;
-	memset(s1, 0, 250);
-	memset(s2, 0, 250);
-	assert(ft_memcpy(s1, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 250) == s1);
-	assert(memcpy(s2, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 250) == s2);
-	while (i < 250)
+	while (i < NTEST)
 	{
-		assert(s1[i] == s2[i]);
-		i++;
-	}
-}
-
-void	test_ft_memcpy_1(void)
-{
-	char s1[50];
-	char s2[50];
-	int i;
-
-	printf(" [*] test on : %s\n", __FUNCTION__);
-	i = 0;
-	memset(s1, 0, 50);
-	memset(s2, 0, 50);
-	assert(ft_memcpy(s1, "abcdefg", 7) == s1);
-	assert(memcpy(s2, "abcdefg", 7) == s2);
-	while (i < 50)
-	{
-		assert(s1[i] == s2[i]);
+		size = rand() % 1000;
+		if (size < 0)
+			size = size * -1;
+		if (!(destft = malloc(size)))
+			return ;
+		if (!(destmemcpy = malloc(size)))
+			return ;
+		memset(destft, 0, size);
+		memset(destmemcpy, 0, size);
+		src = random_junk(size);
+		memcpy(destmemcpy, src, size);	
+		memcpy(destft, src, size);	
+		assert(memcmp(destft, destmemcpy, size) == 0);
+		free(src);
+		free(destmemcpy);
+		free(destft);
 		i++;
 	}
 }
